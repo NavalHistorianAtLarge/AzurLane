39fa7714aeff701eaee271dbc1b5431380b1bc28
NavalHistorianAtLarge <91217150+NavalHistorianAtLarge@users.noreply.github.com>
2025-11-15 20:23:20 -0500

    Update spine character asset format in chibi.js

--- chibi/chibi.js
+++ chibi/chibi.js
@@ -8,7 +8,7 @@ document.body.appendChild(app.view);
 let spineChar;
 
 PIXI.Loader.shared
-  .add('zuikaku', '/chibi/assets/spine/zuikaku/zuikaku.json')
+  .add('zuikaku', '/chibi/assets/spine/zuikaku/zuikaku.skel')
   .load((loader, resources) => {
     spineChar = new PIXI.spine.Spine(resources.zuikaku.spineData);
     spineChar.x = 300;
@@ -24,6 +24,7 @@ function animateZuikaku(action) {
     spineChar.state.setAnimation(0, action, false);
   }
 }
+
 
 
 
