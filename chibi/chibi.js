098c4a6d2fb30bd117a174ade19c005f7809d7c1
NavalHistorianAtLarge <91217150+NavalHistorianAtLarge@users.noreply.github.com>
2025-12-25 16:42:52 -0500

    Update chibi.js

--- chibi/chibi.js
+++ chibi/chibi.js
@@ -448,6 +448,7 @@ document.querySelectorAll('[data-filter]').forEach(btn => {
     }
 
     
+    
     applyFilters();
   });
 });
@@ -519,8 +520,16 @@ if (isMetaFactionSelected()) {
     return true;
   });
 
+
+  if (value === "all" && category === "faction") {
+  activeFilters.faction.clear();
+  document.querySelectorAll('[data-category="faction"]').forEach(b => b.classList.remove('active'));
+  applyFilters();
+  return;
+}
   renderChibiList(results);
 }
+
 
 
 
